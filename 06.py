f119850c184acff86e6ba3e128ce61e9648c552a
Filip Piatek <mrcyberniki@gmail.com>
2020-08-10 18:56:48 -0400

    Ex 6 complete

--- 06.py
+++ 06.py
@@ -1,10 +1,12 @@
 # Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
 
 text = input("Give me a word to check if its a palindrome or not.")
-i = 0
+palindrome = []
 for c in text:
-    if text[i] == text[i - len(text)]:
-        print("Hi")
-        continue
-    else:
-        print(f"{text} is not a palindrome")
+    palindrome.insert(0, c)
+
+palindrome = ''.join(map(str, palindrome))
+if palindrome == text:
+    print("Your word is a palindrome")
+else:
+    print("Your word is not a palindrome")
